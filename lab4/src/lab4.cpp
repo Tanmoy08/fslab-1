a802ec0b92990f0980cae2a0223fcbbc939c266a
Tanmoy Paul <tanmoypaul0805@gmail.com>
2016-03-07 10:03:56 +0530

    Update lab4.cpp

--- lab4/src/lab4.cpp
+++ lab4/src/lab4.cpp
@@ -2,7 +2,11 @@
 #include<vector>
 #include<fstream>
 #include<sstream>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sstream>
 #include<stdlib.h>
+#include<fcntl.h>
 using namespace std;
 vector<int> rrn;
 int rsize=0;
@@ -44,28 +48,76 @@ class Studentrecord{
                int a=buffer.size();
                return a;
         }
-        void write()
+        int write()
 	{
 		fstream fp1;
 		fp1.open("data1.txt",ios::out|ios::app);
 		fp1<<buffer;
-                fp1.close();
-		return;
+                int pos=fp1.tellp();
+                cout<<"pos:"<<pos<<endl;
+                 fp1.close();
+                 return pos;
 		
 	}
-        int prit()
+        void search(int recno)
         {
-	cout<<"inside print"<<endl;
             fstream fp1;
-            fp1.open("data1.txt",ios::in);
-            int pos=fp1.tellp();
+            int a=rrn[recno];
+            cout<<"urrent record position : "<<a<<endl;
+            int b=rrn[recno+1];
+            cout<<"next record position : "<<b<<endl;
+            int len=b-a;
+            cout<<"length of the record : "<<endl;
+            fp1.open("data1.txt");
+            fp1.seekp(a,ios::beg);
+            getline(fp1,buffer);
+            cout<<"Student record is :"<<endl;
+            unpack(len);
             fp1.close();
-            return pos;
         }
+        void unpack(int len)
+        {
+            string s;
+            int i=0;
+            usn.erase();
+            while(buffer[i]!='|')
+            {
+            	usn+=buffer[i];
+            	i++;
+            }
+            i++;
+            name.erase();
+            while(buffer[i]!='|')
+            {
+            	name+=buffer[i];
+            	i++;
+            }
+            i++;
+            branch.erase();
+            while(buffer[i]!='|')
+            {
+		branch+=buffer[i];
+		i++;
+            }
+            i++;
+            sem=0;
+            while(i!=len)
+            {
+            	s=buffer[i];
+            	stringstream convert(s);
+		convert>>sem;
+		i++;
+            }
+            cout<<"usn is :"<<usn<<endl;
+            cout<<"name is :"<<name<<endl;
+            cout<<"branch is :"<<branch<<endl;
+            cout<<"sem is :"<<sem<<endl;
+	    }
 };
 int main()
-{
-    int choice;
+{   
+    fstream fp1;
+    int choice,rec;
     Studentrecord s;
     cout<<"Enter your choice :\n1) Insert\n2) Search"<<endl;
     cin>>choice;
@@ -73,14 +125,43 @@ int main()
     {
         case 1:
         {
+            string s1;
+            fp1.open("data1.txt");
+            fp1.seekg(0,ios::beg);
+            //fp1<<rsize;
+            s1=fp1.get();
+            cout<<"s1:"<<s1<<endl;
+            stringstream convert(s1);
+            convert>>rsize;cout<<"initial vlue of rsize :"<<rsize<<endl;
+            fp1.close();
+            fp1.open("data1.txt");
+            fp1.seekp(0,ios::beg);
+            fp1<<rsize;
+            fp1.close();
             s.read();
             int len=s.pack();
-            s.write();
-            int pos=s.prit();
-            rrn[rsize]=pos-len;
-            for(int i=0;i<=rrn.size();i++)
-                cout<<rrn[i]<<endl;
-           // cout<<"record is successfully written"<<endl;
+            int pos=s.write();
+            cout<<"back"<<endl;
+            
+            rrn.push_back(pos-len);
+            
+            cout<<"rsize is:"<<rsize<<endl;
+            for(int i=rsize;i>=0;i--)
+                cout<<"element"<<i<<"is : "<<rrn[i]<<endl;
+            rsize++;
+            cout<<"value of rsize is : "<<rsize<<endl;
+            fp1.close();
+            fp1.open("data1.txt");
+            fp1.seekp(0,ios::beg);
+            fp1<<rsize;
+            fp1.close();
+        }
+        break;
+        case 2:
+        {
+            cout<<"enter the record number to be shown"<<endl;
+            cin>>rec;
+            s.search(rec);
         }
         break;
         default: cout<<"invalid option chosen"<<endl;
